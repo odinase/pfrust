3fc97fcd54e1047cb1059bec5bc6278c34c4754b
Odin <odin@severinsen.no>
2020-04-02 16:58:40 +0200

    Starting to define interface

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,11 +2,24 @@ use rand::thread_rng;
 use rand_distr::{Distribution, Triangular, TriangularError};
 
 
-fn main() -> Result<(), TriangularError> {
-    let mut rng = thread_rng();
-    let tri_pdf = Triangular::new(-5.0, 5.0, 0.0)?;
-    for _ in 0..10 {
-        println!("Sample is: {}", tri_pdf.sample(&mut rng));
-    }
-    Ok(())
+// fn main() -> Result<(), TriangularError> {
+//     let mut rng = thread_rng();
+//     let tri_pdf = Triangular::new(-5.0, 5.0, 0.0)?;
+//     for _ in 0..10 {
+//         println!("Sample is: {}", tri_pdf.sample(&mut rng));
+//     }
+//     Ok(())
+// }
+
+fn func() -> i32 {
+    3
+}
+
+fn test_closures(func: impl FnOnce() -> i32) -> i32 {
+    return func();
+}
+
+fn main() {
+    println!("func gives: {}", test_closures(func));    
+    println!("func gives: {}", test_closures(func));    
 }
--- src/particles.rs
+++ src/particles.rs
@@ -1,3 +1,26 @@
-pub fn testing() {
-    println!("Testing!");
+/** General trait for methods of Particles:
+ * predict()
+ * update()
+ */
+pub trait Particle {
+    /**
+    * Create new particle
+    */
+    fn new(measurement_model: impl Fn() ->) -> Self;
+    /**
+    * Propagates the particle according to the process model, assuming no noise
+    */
+    fn predict(&mut self);
+    /**
+    * Runs full update, where the weights of the particle is updated according to the measurement model
+    */
+    fn update(&mut self);
+}
+
+pub struct PendulumParticle {}
+
+impl Particle for PendulumParticle {
+    fn new() -> PendulumParticle {PendulumParticle{}}
+    fn predict(&mut self) {}
+    fn update(&mut self) {}
 }
